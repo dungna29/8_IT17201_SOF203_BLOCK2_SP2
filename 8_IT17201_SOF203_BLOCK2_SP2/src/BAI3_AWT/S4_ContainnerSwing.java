5750b2c9e95a2c21ace8cd14e1dde28928573b59
dungna29 <dungna29@localhost>
2022-03-12 18:23:16 +0700

    Dungna 2022

---
+++ 8_IT17201_SOF203_BLOCK2_SP2/src/BAI3_AWT/S4_ContainnerSwing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAI3_AWT;
+
+/**
+ *
+ * @author dungna29
+ */
+public class S4_ContainnerSwing {
+    /*
+        Có 2 kiểu Container trong Swing đó là Top-Level Container và Multi-Purpose Container
+        Top-Level Container bao gồm: Jframe, JDialog, JApplet(Được sử dụng trong ứng dụng WEB), JWindow: Chỉ là một màn hình chờ được bật lên lúc khởi động
+        Multi-Purpose Container gồm: Jpanel, Jlayered, JinternalFrame và JDesktopPane  
+        */        
+}
---
+++ 8_IT17201_SOF203_BLOCK2_SP2/src/BAI3_AWT/S5_PhuongThucJFrame.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAI3_AWT;
+
+/**
+ *
+ * @author dungna29
+ */
+public class S5_PhuongThucJFrame {
+    /*
+        Một vài phương thức trong Jframe
+        setSize(width, height);
+        setLocationRelativeTo(null); //Frame khi chạy sẽ hiển thị ra giữa màn hình
+        setDefaultCloseOperation(EXIT_ON_CLOSE);//tự động đóng khi thoát form khi người dùng click vào nút close
+        setSize(300, 200);
+        setResizable(false); //không cho thay đổi kích thước màn hình
+        setVisible(true);//Hiển thị Jframe trên màn hình khi cần gọi hiển thị   
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);Không làm gì cả
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE); Chỉ đóng frame đó, các frame khác liên quan sẽ không bị đóng       
+        setLocation(x,y); Đặt vị trí xuất hiện trên màn hình
+        pack(); Đặt kích thước Jrame vừa đủ với nội dung
+        getContentPane().SetBackgroud(Corlor."Color");
+        */
+}
